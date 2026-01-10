97005463d36fe3a914aee3c4177ab5d89d391667
Chrysanthos Kattimeris <ckattimeris@Chrysanthoss-Mac-mini-682.local>
2026-01-10 19:01:12 +0200

    fix dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,62 +2,86 @@
 
 @section('title', 'Dashboard')
 
-@section('content-header')
-<div class="row mb-2">
-    <div class="col-sm-6">
-        <h1 class="m-0">Dashboard</h1>
-    </div>
-</div>
-@endsection
-
 @section('content')
-<div class="row">
-    <div class="col-lg-4 col-12">
-        <div class="small-box bg-info">
-            <div class="inner">
-                <h3>0</h3>
-                <p>Today Income</p>
-            </div>
-            <div class="icon">
-                <i class="fas fa-coins"></i>
-            </div>
-            <a href="#" class="small-box-footer">Coming soon <i class="fas fa-arrow-circle-right"></i></a>
+<div class="container-fluid">
+
+    <div class="card mb-3">
+        <div class="card-body">
+            <h1 class="h3 mb-1">
+                {{ $greeting }}, {{ $displayName }} 👋
+            </h1>
+            <p class="text-muted mb-0">
+                You’re logged in. Next we’ll build Income / Expenses / Accounts screens (front-end first).
+            </p>
         </div>
     </div>
 
-    <div class="col-lg-4 col-12">
-        <div class="small-box bg-warning">
-            <div class="inner">
-                <h3>0</h3>
-                <p>Today Expenses</p>
-            </div>
-            <div class="icon">
-                <i class="fas fa-file-invoice-dollar"></i>
-            </div>
-            <a href="#" class="small-box-footer">Coming soon <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+    <div class="row">
 
-    <div class="col-lg-4 col-12">
-        <div class="small-box bg-success">
-            <div class="inner">
-                <h3>0</h3>
-                <p>Account Balance</p>
+        {{-- Today Income --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ number_format((float)$todayIncome, 2) }}</h3>
+                    <p>Today Income</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-coins"></i>
+                </div>
+                <a href="{{ route('income.index') }}" class="small-box-footer">
+                    View Income <i class="fas fa-arrow-circle-right"></i>
+                </a>
             </div>
-            <div class="icon">
-                <i class="fas fa-university"></i>
-            </div>
-            <a href="#" class="small-box-footer">Coming soon <i class="fas fa-arrow-circle-right"></i></a>
         </div>
-    </div>
-</div>
 
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title">Welcome</h3>
-    </div>
-    <div class="card-body">
-        You’re logged in. Next we’ll build Income / Expenses / Accounts screens (front-end first).
+        {{-- Today Expenses --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ number_format((float)$todayExpenses, 2) }}</h3>
+                    <p>Today Expenses</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-file-invoice-dollar"></i>
+                </div>
+                <a href="{{ route('expenses.index') }}" class="small-box-footer">
+                    View Expenses <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        {{-- Month Income --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ number_format((float)$monthIncome, 2) }}</h3>
+                    <p>Total Month Income</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-chart-line"></i>
+                </div>
+                <a href="{{ route('income.index') }}" class="small-box-footer">
+                    View Income <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        {{-- Month Expenses --}}
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-warning text-white">
+                <div class="inner">
+                    <h3 class="text-white">{{ number_format((float)$monthExpenses, 2) }}</h3>
+                    <p class="text-white">Total Month Expenses</p>
+                </div>
+                <div class="icon text-white">
+                    <i class="fas fa-receipt"></i>
+                </div>
+                <a href="{{ route('expenses.index') }}" class="small-box-footer text-white">
+                    View Expenses <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
